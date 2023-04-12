a1610c12e6b8e0c0f2e502941804f37ecba825a2
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2023-04-12 09:38:45 +0800

    finish

--- react-js/dots-project/src/App.tsx
+++ react-js/dots-project/src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   // update h1 with increment of 1
   const [number, setNumber] = useState(0);
   // press button to console log error
-  function handleButtonClick() {
-    setNumber(number + 1);
+  function handleButtonClick(delta: number) {
+    setNumber(number + delta);
   }
 
   return (
@@ -15,10 +15,10 @@ function App() {
         <h1>{number}</h1>
       </div>
       <div>
-        <button onClick={handleButtonClick}>-10</button>
-        <button onClick={handleButtonClick}>-1</button>
-        <button onClick={handleButtonClick}>+1</button>
-        <button onClick={handleButtonClick}>+10</button>
+        <button onClick={() => handleButtonClick(-10)}>-10</button>
+        <button onClick={() => handleButtonClick(-1)}>-1</button>
+        <button onClick={() => handleButtonClick(1)}>+1</button>
+        <button onClick={() => handleButtonClick(10)}>+10</button>
       </div>
     </div>
   );
